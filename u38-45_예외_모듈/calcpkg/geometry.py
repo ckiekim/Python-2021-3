5992bbd8413b3b345fa4a99769cab4f7732a4102
Cheolkie Kim <ckiekim@naver.com>
2021-08-06 09:18:12 +0900

    심사문제

---
+++ u38-45_예외_모듈/calcpkg/geometry.py
@@ -0,0 +1,7 @@
+import math
+ 
+def circle_area(radius):
+    return radius * radius * math.pi
+
+if __name__ == '__main__':
+    print(circle_area(2))
---
+++ u38-45_예외_모듈/calcpkg/operation.py
@@ -0,0 +1,7 @@
+import math
+ 
+def squareroot(n):
+    return math.sqrt(n)
+
+if __name__ == '__main__':
+    print(squareroot(2))
